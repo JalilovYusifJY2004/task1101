0a945792f1bc0f72af434d00b7015e4beda290a5
JalilovYusifJY2004 <yusifcelilli2004@gmail.com>
2023-10-10 21:50:45 +0400

    add

---
+++ task0111/task0111/Program.cs
@@ -0,0 +1,112 @@
+﻿namespace task0111
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //task2
+            {
+                //Student student1 = new Student("Yusif", "Celilli", 20, true);
+                //Student student2 = new Student("Xeyal ", "Huseynov", 20, false);
+                //Student student3 = new Student("Kenan", "Pasayev", 19, true);
+
+                //Group group = new Group("Ab104 Class");
+                //group.Add(student1);
+                //group.Add(student2);
+                //group.Add(student3);
+
+                //group.GetAll();
+                //group.GetOnlineStudents();
+                //group.GetOfflineStudents();
+
+            }
+
+        }
+        //task2
+        //class Person
+        //{
+        //    public string Name { get; }
+        //    public string Surname { get; }
+        //    public int Age { get; }
+
+        //    public Person(string name, string surname, int age)
+        //    {
+        //        Name = name;
+        //        Surname = surname;
+        //        Age = age;
+        //    }
+        //}
+
+        //class Student : Person
+        //{
+        //    public bool IsOnline { get; }
+
+        //    public Student(string name, string surname, int age, bool isOnline)
+        //        : base(name, surname, age)
+        //    {
+        //        IsOnline = isOnline;
+        //    }
+        //}
+
+        //class Group
+        //{
+        //    public string GroupName { get; }
+        //    private List<Student> Students { get; }
+
+        //    public Group(string groupName)
+        //    {
+        //        GroupName = groupName;
+        //        Students = new List<Student>();
+        //    }
+
+        //    public void Add(Student student)
+        //    {
+        //        Students.Add(student);
+        //    }
+
+        //    public void GetAll()
+        //    {
+        //        Console.WriteLine("All Students in Group:");
+        //        foreach (var student in Students)
+        //        {
+        //            Console.WriteLine($"{student.Name} {student.Surname}");
+        //        }
+        //    }
+
+        //    public void GetOnlineStudents()
+        //    {
+        //        Console.WriteLine("Online Students in Group:");
+        //        foreach (var student in Students)
+        //        {
+        //            if (student.IsOnline)
+        //            {
+        //                Console.WriteLine($"{student.Name} {student.Surname}");
+        //            }
+        //        }
+        //    }
+
+        //    public void GetOfflineStudents()
+        //    {
+        //        Console.WriteLine("Offline Students in Group:");
+        //        foreach (var student in Students)
+        //        {
+        //            if (!student.IsOnline)
+        //            {
+        //                Console.WriteLine($"{student.Name} {student.Surname}");
+        //            }
+        //        }
+        //    }
+        //}
+
+       
+            }
+        }
+    
+
+
+
+
+
+
+
+
