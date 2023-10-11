8769ae3627c1dcaf7ef4b76b186480378be28809
Yusif <145650476+JalilovYusifJY2004@users.noreply.github.com>
2023-10-11 06:13:19 +0400

    Update Program.cs

--- task0111/task0111/Program.cs
+++ task0111/task0111/Program.cs
@@ -6,6 +6,19 @@
         {
            //task2
             {
+                //TASK-1
+            //Animal animal = new Animal("");
+
+            //Console.WriteLine(animal.BirthDate);
+
+            //Dog animal1 = new Dog("Toplan", "Kangal", "Erkek");
+
+            //Console.WriteLine(animal1._breed);
+
+            //TASK-2
+
+            //Student student=new Student("Xeyal", "Huseynov", 20,true);
+            //Console.WriteLine(student);
                 //Student student1 = new Student("Yusif", "Celilli", 20, true);
                 //Student student2 = new Student("Xeyal ", "Huseynov", 20, false);
                 //Student student3 = new Student("Kenan", "Pasayev", 19, true);
@@ -22,6 +35,39 @@
             }
 
         }
+         //TASK-1
+    //class Animal
+    //{
+    //    public string _gender { get; }
+    //    private DateTime _birthDate;
+
+    //    public DateTime BirthDate
+    //    {
+    //        get { return _birthDate; }
+    //    }
+
+    //    public Animal(string gender)
+    //    {
+    //        _gender = gender;
+    //        _birthDate = DateTime.Now;
+
+    //        _birthDate= DateTime.Now;
+    //    }
+
+    //}
+
+    //class Dog:Animal
+    //{
+    //    public string _name { get; }
+    //    public string _breed { get; }
+
+    //    public Dog(string name, string breed, string gender) : base(gender)
+    //    {
+    //        _name = name;
+    //        _breed = breed;
+
+    //        _breed = "Kangal";
+    //    }
         //task2
         //class Person
         //{
